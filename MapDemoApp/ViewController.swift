830a8b3b805941101f7ad50cadb55cd5d3b26dc5
KitaharaMugiro <ngthornym@yahoo.co.jp>
2019-03-10 01:29:27 +0900

    Initial Commit

---
+++ MapDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapDemoApp
+//
+//  Created by kitaharamugirou on 2019/03/10.
+//  Copyright © 2019 kitaharamugirou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
