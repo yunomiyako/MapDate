5a646cb7eb6e37a4831de31e3286fd83cb079966
Your Name <tcu>
2019-03-16 12:31:55 +0900

    レイアウト修正

--- MapDemoApp/VC/MyProfile/ProfileEditViewController.swift
+++ MapDemoApp/VC/MyProfile/ProfileEditViewController.swift
@@ -18,42 +18,69 @@ final class EditProfileViewController: UIViewController {
     let navigationView = UIView()
     var state = ""
     var profimgs = [UIImageView]()
-    
+    var profimgBtns = [UIButton]()
+    var tmp = false
     var doneButtonTapHandler: ((String) -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-
         
-        scrview.contentSize = CGSize(width: 300, height: 1400)
         scrview.indicatorStyle = .black
         scrview.backgroundColor = UIColor.white.dark()
         self.view.addSubview(scrview)
         
-        for i in 0..<1{
-            profimgs.append(UIImageView(frame: CGRect(x:30 , y: 30, width: 60, height: 100)))
+        for i in 0..<6{
+            profimgs.append(UIImageView())
             profimgs[i].image = UIImage(named: "frame.png")
             scrview.addSubview(profimgs[i])
+            profimgBtns.append(UIButton())
+            profimgBtns[i].setImage(UIImage(named:"addimg.png"), for: UIControl.State.normal)
+            scrview.addSubview(profimgBtns[i])
         }
+
         
         textEditer.backgroundColor = UIColor.white
         textEditer.text = state
         self.scrview.addSubview(textEditer)
       
         
-        
-        
- 
-        
+
     }
     
     override func viewDidLayoutSubviews(){
         super.viewDidLayoutSubviews()
-  
+
         scrview.scrollIndicatorInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         scrview.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        textEditer.frame = CGRect(x: 0, y: self.scrview.frame.height - 500, width: self.view.frame.width, height: 500)
+        scrview.contentSize = CGSize(width:0, height: scrview.frame.height * 1.5)
+        let scrhalfY = self.scrview.frame.height * 0.5
+        let scrhalfX = self.scrview.frame.width * 0.5
+        let prfpos1 = scrhalfY * 0.25
+        
+        for i in 0..<6{
+            
+            if i < 3{
+                profimgs[i].frame = CGRect(x: 0, y: prfpos1, width: self.scrview.frame.width * 0.22, height: scrhalfY * 0.3)
+                profimgBtns[i].frame = CGRect(x: 0, y: prfpos1 * 2, width: profimgs[i].frame.width * 0.3, height: profimgs[i].frame.width * 0.3)
+            }else{
+                profimgs[i].frame = CGRect(x: 0, y: prfpos1 + (scrview.frame.width * 0.22)+prfpos1 * 0.5, width: self.scrview.frame.width * 0.22, height: scrhalfY * 0.3)
+                profimgBtns[i].frame = CGRect(x: 0, y:prfpos1 * 2 + scrhalfY * 0.35, width: profimgs[i].frame.width * 0.3, height: profimgs[i].frame.width * 0.3)
+            }
+            switch (i+1)%3{
+            case 1:
+                profimgs[i].frame.origin.x = scrhalfX * 0.2
+            case 2:
+                profimgs[i].frame.origin.x = scrhalfX * 0.8
+            case 0:
+                profimgs[i].frame.origin.x = scrhalfX * 1.4
+            default: print("err")
+            }
+            profimgBtns[i].frame.origin.x = profimgs[i].frame.minX + profimgs[i].frame.width * 0.75
+        }
+        
+        
+        textEditer.frame = CGRect(x: 0, y: self.scrview.frame.height - (scrhalfY), width: self.view.frame.width, height: (scrhalfY))
+
         addNavBackView()
         addNavigationBar()
     }
